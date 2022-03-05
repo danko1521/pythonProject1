7f207ca1bc8614b091587f38b54c508a3d023fe0
boy <71589142+danko1521@users.noreply.github.com>
2022-03-05 16:57:39 +0300

    Update Decorator.py

--- Decorator.py
+++ Decorator.py
@@ -1,45 +1,61 @@
+import os
 import functools
 from datetime import datetime
-import logging
 
 nested_list = [
-    ['a', 'b', False, 'c', 'gig', [12, 13]],
-    ['d', 'e', 'f', 'h', False],
-    ['p', 'i', 'g', 'g'],
-    [1, 2, 3, 4, 5]
+    ['a', 'b', 'c'], [1, 2, 3, 4, 5, 6, 7, 8], ['d', 'e', 'f', 'h', False], ['d', 'e', 'f', 'h', False], [1, 2, None]
 ]
 
 
-def debug(func):
-    @functools.wraps(func)
-    def wrapper_debug(*args, **kwargs):
-        args_repr = [repr(a) for a in args]
-        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
-        signature = ", ".join(args_repr + kwargs_repr)
-        print(f"вызывается функция {func.__name__} с аргументом({signature})")
-        value = func(*args, **kwargs)
-        print(f"функция {func.__name__!r} вернула аргумент {value!r}")
-        now = datetime.now()
-        current_time = now.strftime("%y:%m:%d:%H:%M:%S")
-        with open("loger.txt", "w") as file:
-            file.write(f'time calling function {current_time}, name func {func.__name__},'
-                       f' func return argument {signature} and return value {value}')
-        return value
+def param_decor(param):
+    def actual_debug_decor(func):
+        @functools.wraps(func)
+        def wrapper_debug(*args, **kwargs):
+            args_repr = [repr(a) for a in args]
+            kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+            signature = ", ".join(args_repr + kwargs_repr)
+            print(f'находится в дериктории - {param}')
+            print(f"вызывается функция {func.__name__} с аргументом({signature})")
+            value = func(*args, **kwargs)
+            print(f"функция {func.__name__!r} вернула аргумент {value!r}")
+            now = datetime.now()
+            current_time = now.strftime("%y:%m:%d:%H:%M:%S")
+            with open("loger.txt", "w") as file:
+                file.write(f'time calling function {current_time}, name func {func.__name__},'
+                           f' func return argument {signature} and return value {value}')
+            return value
 
-    return wrapper_debug
+        return wrapper_debug
 
-
-@debug
-def some_list_of_list(nested_list):
-    for some_list in nested_list:
-        for elem in some_list:
-            if bool(elem) in some_list:
-                elem = str(elem)
-            for el in elem:
-                yield el
-    flattened = [elem for list in nested_list for elem in list]
-    print(flattened)
+    return actual_debug_decor
 
 
-for item in some_list_of_list(nested_list):
-    print(item)
+@param_decor(param=os.path)
+class flat_list:
+
+    def __init__(self):
+        self.list = nested_list
+        self.flat_list = []
+
+    def __iter__(self):
+        self.count = 0
+        return self
+
+    def __next__(self):
+        self.count += 1
+        flat_list = []
+
+        for element in nested_list:
+            if type(element) is list:
+
+                for item in element:
+                    flat_list.append(item)
+            else:
+                flat_list.append(element)
+        return flat_list
+
+
+List_iter = flat_list()
+for reading in List_iter:
+    print(reading)
+    break
